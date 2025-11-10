010fa6746aa7cb4174e3f9271b0443e5a76ea8a8
kilfour <kilfour@gmail.com>
2025-11-10 15:33:36 +0100

    doc header allowed on class

--- QuickPulse.Explains/DocHeaderAttribute.cs
+++ QuickPulse.Explains/DocHeaderAttribute.cs
@@ -7,7 +7,7 @@ namespace QuickPulse.Explains;
 /// The optional level offset allows fine-tuning of the header depth relative to the surrounding structure,
 /// enabling nested sections or subheaders within a [DocFile] or method documentation block.
 /// </summary>
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
 public class DocHeaderAttribute(string header, int level = 0) : DocFragmentAttribute
 {
     public string Header { get; } = header;
